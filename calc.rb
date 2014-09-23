02f63abe79fd42c8c2939a8d97dc8c4902bd76e2
raielin@gmail.com <raielin@gmail.com>
2014-09-23 19:27:52 -0400

    Initial code setup with notes on primary required methods/functions and inputs/outputs.

--- calc.rb
+++ calc.rb
@@ -1,1 +1,16 @@
-# Your code goes here
+# Practice with functions, loops, conditions, user-input
+# Build a calculator: A calculator can perform multiple arithmetic operations. Your function should allow the user to choose which operation is expected, enter in the values to perform the operation on, and ultimately view the result.
+
+require 'pry'
+
+# prompt user for choice of calculator function
+
+# user provides values to perform operation on
+    # for division and subtraction - will need to specify numerator and denominator in division; subtraction....as addition of a negative number?
+
+# outputs string of the equation user is asking to run.
+# ask to confirm equation. if confirmed, run equation and output result. if not confirmed, prompt for new inputs.
+
+
+
+
